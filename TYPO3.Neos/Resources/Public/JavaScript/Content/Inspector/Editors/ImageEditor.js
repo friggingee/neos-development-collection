cbfe8128791bc6810683c4b9eacaa9c3b3951de6
Aske Ertmann <aertmann@gmail.com>
2016-05-21 16:00:24 +0200

    TASK: Display notificaitons from media browser iframe

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/Editors/ImageEditor.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/Editors/ImageEditor.js
@@ -327,8 +327,15 @@ function (Ember, $, FileUpload, template, cropTemplate, BooleanEditor, Spinner, 
 						that._hideImageLoader();
 					});
 				},
-				onLoad: function() {
+				onLoad: function(event, iframe) {
 					this._frameLoaded = true;
+					var notifications = $(iframe).contents().find('#neos-notifications-inline');
+					if (notifications.length > 0) {
+						$('li', notifications).each(function(index, notification) {
+							var title = $(notification).data('title');
+							Notification[$(notification).data('type')](title ? title : $(notification).text(), title ? $(notification).html() : '');
+						});
+					}
 				},
 				refreshThumbnail: function() {
 					if (this._frameLoaded) {
@@ -356,7 +363,7 @@ function (Ember, $, FileUpload, template, cropTemplate, BooleanEditor, Spinner, 
 				didInsertElement: function() {
 					this.$().find('iframe').on('load', function(event) {
 						if (window.Typo3MediaBrowserCallbacks && window.Typo3MediaBrowserCallbacks.onLoad) {
-							window.Typo3MediaBrowserCallbacks.onLoad(event);
+							window.Typo3MediaBrowserCallbacks.onLoad(event, this);
 						}
 					});
 				}
@@ -1070,7 +1077,7 @@ function (Ember, $, FileUpload, template, cropTemplate, BooleanEditor, Spinner, 
 				didInsertElement: function() {
 					this.$().find('iframe').on('load', function(event) {
 						if (window.Typo3MediaBrowserCallbacks && window.Typo3MediaBrowserCallbacks.onLoad) {
-							window.Typo3MediaBrowserCallbacks.onLoad(event);
+							window.Typo3MediaBrowserCallbacks.onLoad(event, this);
 						}
 					});
 				}
